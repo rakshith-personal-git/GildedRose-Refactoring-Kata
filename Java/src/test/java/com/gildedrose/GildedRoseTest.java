e5328e00e5285354da1ddbacabe0995c0a53cb91
Rakshith A <rakshith.a@moneyview.in>
2026-03-06 23:21:25 +0530

    first temp commit

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,6 +12,7 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+        //assertEquals("foo", app.items[0].name);
     }
 
 }
